d6f5d12de385130d3e89d2041b22f3e0fdbdfcc0
Kaede Fujisaki <kaede@hexe.net>
2020-10-23 20:44:12 +0900

    remove helper

--- helper/build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -eu
-
-PROJ_PATH=$(cd "$(dirname "$(readlink -f $0)")" && pwd)
-cd "${PROJ_PATH}/.."
-
-mkdir -p artifacts
-
-docker build -f 'Dockerfile' .
-docker run --rm -v "${PWD}/artifacts:/artifacts" --entrypoint="cp" KeroKeroRepeat "/KeroKeroRepeat" "/artifacts"
